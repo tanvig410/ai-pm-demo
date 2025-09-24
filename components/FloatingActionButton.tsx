c90ce62d7f9bc5f9b4eae909f9553ff072210663
tanvig410 <88389508+tanvig410@users.noreply.github.com>
2025-09-24 15:24:24 +0530

    Delete components directory

--- components/FloatingActionButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Plus } from 'lucide-react';
-
-export function FloatingActionButton() {
-  return (
-    <button className="fixed bottom-8 right-8 z-50 w-14 h-14 bg-[#00C2FF] rounded-[8px] flex items-center justify-center hover:bg-[#00B8F0] transition-colors shadow-lg">
-      <Plus className="h-6 w-6 text-white" />
-    </button>
-  );
-}
